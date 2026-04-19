699f92ebe62db66f27169bb09b33ebf324625990
tohellebust <torgeir.hellebust@gmail.com>
2026-04-19 21:43:19 +0200

    feat(web): list and activate cases from the UI

---
+++ CaseSetup.Web/Controllers/CasesController.cs
@@ -0,0 +1,29 @@
+﻿using CaseSetup.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Contracts.Dtos;
+
+namespace CaseSetup.Web.Controllers;
+
+public class CasesController : Controller
+{
+    private readonly ICaseApiClient _caseApiClient;
+
+    public CasesController(ICaseApiClient caseApiClient)
+    {
+        _caseApiClient = caseApiClient;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+       IReadOnlyList<CaseScenarioDto> cases = await _caseApiClient.GetAllCasesAsync();
+       return View(cases);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Activate(int id)
+    {
+        await _caseApiClient.ActivateAsync(id);
+
+        return RedirectToAction(nameof(Index));
+    }
+}
