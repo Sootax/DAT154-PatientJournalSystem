6bea8ab0d1f0980bcebfb9e255df9cf3f013c984
tohellebust <torgeir.hellebust@gmail.com>
2026-04-20 14:38:40 +0200

    Add authentication endpoint and seed users in Backend.Api

---
+++ Backend.Api/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Infrastructure.Persistence;
+using Shared.Contracts.Requests;
+using Shared.Contracts.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public AuthController(AppDbContext db)
+    {
+        this._db = db;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginRequest loginRequest)
+    {
+        var user = await _db.Users.FirstOrDefaultAsync((user) => user.Username == loginRequest.Username);
+
+        if (user is null)
+        {
+            return Unauthorized();
+        }
+
+        if (user.PasswordHash != loginRequest.Password)
+        {
+            return Unauthorized();
+        }
+
+        var userDto = new UserDto
+        {
+            UserId = user.UserId,
+            Username = user.Username,
+            Role = user.Role
+        };
+
+        return Ok(userDto);
+    }
+}
