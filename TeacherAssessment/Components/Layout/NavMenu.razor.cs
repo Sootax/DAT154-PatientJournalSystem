5c80c42e6811ed81e274a079cecfda7cd2e43249
tohellebust <torgeir.hellebust@gmail.com>
2026-04-27 15:26:46 +0200

    feat(teacher): contextual nav menu and rewritten home/not-found

---
+++ TeacherAssessment/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using TeacherAssessment.Services;
+
+namespace TeacherAssessment.Components.Layout;
+
+public partial class NavMenu : ComponentBase, IDisposable
+{
+    [Inject] private TeacherSessionState SessionState { get; set; } = default!;
+    [Inject] private NavigationManager Navigation { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        SessionState.OnChange += HandleStateChanged;
+    }
+
+    private void HandleStateChanged()
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    private void LogoutAsync()
+    {
+        SessionState.Logout();
+        Navigation.NavigateTo("/");
+    }
+
+    public void Dispose()
+    {
+        SessionState.OnChange -= HandleStateChanged;
+    }
+}
---
+++ TeacherAssessment/Components/Pages/Home.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using TeacherAssessment.Services;
+
+namespace TeacherAssessment.Components.Pages;
+
+public partial class Home : ComponentBase
+{
+    [Inject] private TeacherSessionState SessionState { get; set; } = default!;
+}
